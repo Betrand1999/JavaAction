54ed3b5564ba7e160e10ca58286085a351995522
root <jpoceans.in@gmail.com>
2023-08-17 21:41:07 +0530

    update message

--- src/main/java/com/example/MSB/MsbApplication.java
+++ src/main/java/com/example/MSB/MsbApplication.java
@@ -2,10 +2,17 @@ package com.example.MSB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MsbApplication {
 
+    @GetMapping("/")
+    public String hello() {
+        return "<h1>Hello, world!</h1>";
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(MsbApplication.class, args);
 	}
