84aaaa2ccd6a738a32264ea4e0beafba3a1f95a6
Jai Prakash Kushwaha <71000900+jpoceans-in@users.noreply.github.com>
2023-08-23 21:57:08 +0530

    W

--- src/main/java/com/example/MSB/MsbApplication.java
+++ src/main/java/com/example/MSB/MsbApplication.java
@@ -11,7 +11,11 @@ public class MsbApplication {
 
     @GetMapping("/")
     public String hello() {
+<<<<<<< HEAD
+        return "<h1>Hello, Containers</h1><h3>Execute in Background ...</h3>";
+=======
         return "<h1>Hello, Containers</h1><h3>Execute in Background ..</h3>";
+>>>>>>> 5e4d4c17dc48d54ceaa3efcbfeba4468049fabf7
     }
 	public static void main(String[] args) {
 		SpringApplication.run(MsbApplication.class, args);
